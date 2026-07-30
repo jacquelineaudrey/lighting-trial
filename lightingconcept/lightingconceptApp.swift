a0c1503dea984d481c6a4a32977df0b890971382
Jacqueline <jacquelineaudreyiman@gmail.com>
2026-07-30 13:52:17 +0700

    Initial Commit

---
+++ lightingconcept/lightingconceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightingconceptApp.swift
+//  lightingconcept
+//
+//  Created by Jacqueline on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct lightingconceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
